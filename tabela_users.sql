1844b4a6ad5874adff8fbdb765a7af9e419b8fed
Eduardo <eduardozaffarimonteiro@gmail.com>
2024-05-27 20:53:24 -0300

    procedure insere users

--- tabela_users.sql
+++ tabela_users.sql
@@ -1,13 +1,14 @@
 CREATE TABLE USERS
 (
-    USERID NUMBER,
+    USERID NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
     PASSWORD VARCHAR(32), -- TAMANHO DO HASH MD5
     ID_LIDER CHAR(14 BYTE) UNIQUE,
     CONSTRAINT PK_USERS PRIMARY KEY (USERID),
     CONSTRAINT FK_USERS FOREIGN KEY (ID_LIDER) REFERENCES LIDER(CPI)
-)
+);
+
 
 -- Pedir para os monitores se o ideal é usar o hash em um procedimento de inserção
 -- ou há como fazer na definição da tabela
-insert into users values (0, standard_hash('sehna', 'MD5'), '123.456.789-10');
+insert into users (PASSWORD,ID_LIDER) values (standard_hash('sehna', 'MD5'), '223.456.789-10');
 select * from users;
