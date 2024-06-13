ab847f8e6e8e4f8678867dcfd2bf9002ce55386e
gustavosb <gustavosb64@gmail.com>
2024-06-13 19:50:20 -0300

    Atualizando a procedure de alteracao de nome de faccao

--- SQL/gerenciamento_lider.sql
+++ SQL/gerenciamento_lider.sql
@@ -75,18 +75,55 @@ CREATE OR REPLACE PACKAGE BODY gerenciamento_lider AS
         commit;
     END remove_nacao_faccao;
     
-    -- TODO: ATUALIZAR TABELAS FILHAS
     -- PROCEDURE PARA ALTERAR NOME DA FACCAO
     PROCEDURE alterar_nome_faccao (
         p_cpi           LIDER.CPI%type,
         p_faccao_old    FACCAO.NOME%type,
         p_faccao_new    FACCAO.NOME%type
-    ) AS
+    ) AS    
+        TYPE tb_nacoes_associadas IS TABLE OF Nacao_Faccao.Nacao%type;
+        v_nacoes_associadas tb_nacoes_associadas := tb_nacoes_associadas();
+        
+        TYPE tb_participa IS RECORD (
+            tb_especie  Participa.Especie%type,
+            tb_comunidade  Participa.Comunidade%type
+            
+        );
+        TYPE tb_comunidades_associadas IS TABLE OF tb_participa;
+        v_comunidades_associadas tb_comunidades_associadas := tb_comunidades_associadas();
+        
     BEGIN
+        -- Armazenas as tuplas de PARTICIPA referentes em uma coleção e as deleta
+        SELECT Especie, Comunidade BULK COLLECT INTO v_comunidades_associadas
+            FROM Participa 
+            WHERE Faccao = p_faccao_old;
+        DELETE FROM Participa WHERE Faccao = p_faccao_old; 
+        
+        -- Armazenas as tuplas de NACAO_FACCAO referentes em uma coleção e as deleta
+        SELECT Nacao BULK COLLECT INTO v_nacoes_associadas
+            FROM Nacao_Faccao 
+            WHERE Faccao = p_faccao_old;    
+        DELETE FROM Nacao_Faccao WHERE Faccao = p_faccao_old;  
+        
+        -- Atualiza o nome da Faccao
         UPDATE Faccao 
             SET Nome = p_faccao_new
             WHERE Nome = p_faccao_old;
-        commit;
+    
+        -- Reinsere os valores removidos anteriormente com o nome atualizado
+        FOR i IN v_comunidades_associadas.FIRST .. v_comunidades_associadas.LAST 
+        LOOP
+            INSERT INTO Participa 
+                VALUES (p_faccao_new, v_comunidades_associadas(i).tb_especie, v_comunidades_associadas(i).tb_comunidade);
+        END LOOP;
+        
+        FOR i IN v_nacoes_associadas.FIRST .. v_nacoes_associadas.LAST 
+        LOOP
+            INSERT INTO Nacao_Faccao 
+                VALUES (v_nacoes_associadas(i), p_faccao_new);    
+        END LOOP;
+        
+        COMMIT;
     END alterar_nome_faccao;
     
     -- PROCEDURE PARA INDICAR NOVO LIDER
