5a81128a1a785c893a6de8c128c7fa431568b84c
gustavosb <gustavosb64@gmail.com>
2024-06-14 21:04:11 -0300

    Corrigindo erros de sintaxe.

--- SQL/triggers/tr_comunidade.sql
+++ SQL/triggers/tr_comunidade.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TRIGGER TR_COMUNIDADE_CHECAGEM_INTELIGENTE
 BEFORE INSERT OR UPDATE ON Comunidade
 FOR EACH ROW
 DECLARE
-    v_inteligente ESPECIE.Inteligente%type
+    v_inteligente ESPECIE.Inteligente%type;
 BEGIN
 
     -- Sempre haverá um resultado, já que se trata de chave estrangeira
@@ -12,5 +12,6 @@ BEGIN
 
     IF v_inteligente = 'F' THEN
         RAISE_APPLICATION_ERROR(-20700,'Espécies não inteligentes não podem compor uma comunidade.');
+    END IF;
 
-END NRO_NACOES;
+END TR_COMUNIDADE_CHECAGEM_INTELIGENTE;
