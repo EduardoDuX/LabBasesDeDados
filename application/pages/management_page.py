744aeed1cd9e20ecc841833d4d255ec18b398fed
ale-souza <alesouza0021@gmail.com>
2024-06-16 12:21:27 -0300

    Removendo gerenciamento de líder

--- application/pages/management_page.py
+++ application/pages/management_page.py
@@ -6,60 +6,8 @@ import os
 from datetime import date
 from oracledb import DatabaseError
 
-def lider():
-    st.title('Gerencie sua facção')
 
-    # Alterar o nome da facção
-    with st.container(border=True):
-        st.subheader('Alterar nome da facção')
-        st.text_input(
-            label='Nome da facção',
-            placeholder='Insira o novo nome da facção',
-            key='new_fac_name'
-        )        
-        if st.button('Alterar'):
-            with st.session_state.connection.cursor() as cursor:
-                cursor.callproc(
-                    'lider_faccao.alterar_nome_faccao',
-                    [st.session_state.cpi, st.session_state.faccao, st.session_state.new_fac_name]
-                )
-                st.session_state.faccao = st.session_state.new_fac_name
-
-            st.text('Nome da facção alterado com sucesso!')
-
-    # Indicar um novo líder
-    with st.container(border=True):
-        st.subheader('Indicar um novo líder')
-        st.markdown("<h5 color: white;'>Cuidado! Você será desconectado ao indicar um novo líder</h5>", unsafe_allow_html=True)
-        
-        st.text_input(label='CPI do novo líder', placeholder='Digite o CPI no formato XXX.XXX.XXX-XX', key='new_ldr_fac')
-        if st.button('Indicar'):
-            with st.session_state.connection.cursor() as cursor:
-                cursor.callproc("lider_faccao.indicar_novo_lider", [st.session_state.new_ldr_fac])
-
-            # Ao dar o privilégio de líder da facção para o novo líder, o líder anterior é desconectado
-            st.text('Novo líder indicado com sucesso! Você será desconectado em breve...')
-            time.sleep(5)
-            st.switch_page('login_page.py')
-    
-    # Credenciar uma nova comunidade
-    with st.container(border=True):
-        st.subheader('Credenciar nova comunidade')
-        sub_col1, sub_col2 = st.columns(2)
-        with sub_col1:
-            st.text_input(label='Nome da comunidade', placeholder='Insira o nome da comunidade')
-        with sub_col2:
-            st.text_input(label='Espécie da comunidade', placeholder='Insira a espécide da comunidade')
-        st.button('Credenciar')
-    
-    # Remover facção da Nação
-    with st.container(border=True):
-        st.subheader('Remover facção da Nação')
-        st.text_input(label='Nome da nação', placeholder='Insira o nome da nação', key='rmv_nac')
-        if st.button('Remover'):
-            with st.session_state.connection.cursor() as cursor:
-                cursor.callproc("lider_faccao.remove_nacao_faccao", [st.session_state.rmv_nac])
-
+# Função que organiza as opções de gerenciamento de cientista
 def cientista():
     # Gerencia CRUD de estrelas
     st.title('Gerencie estrelas')
@@ -353,6 +301,7 @@ def cientista():
             st.text('Estrela removida com sucesso!')
 
 
+# Função que organiza as opções de gerenciamento de comandante
 def comandante():
     st.title('Gerencie sua nação e suas dominâncias')
 
@@ -408,13 +357,10 @@ def management_page():
         st.switch_page('pages/main_page.py')
 
     match st.session_state.user_type:
-        case 'LIDER':
-            lider()
         case 'COMANDANTE':
             comandante()
         case 'CIENTISTA':
             cientista()
-
             
     if st.button('Voltar', type='primary', key='v2'):
         st.switch_page('pages/main_page.py')
