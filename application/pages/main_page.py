5d251e382d01eae50761d5a102f5eb22e202219c
ale-souza <alesouza0021@gmail.com>
2024-06-16 12:21:52 -0300

    Adicionando botões específicos para líder

--- application/pages/main_page.py
+++ application/pages/main_page.py
@@ -11,64 +11,81 @@ def main_page():
         st.session_state['user_type'] = None
     
     if 'report_type' not in st.session_state.keys():
-        st.session_state['report_type'] = None 
+        st.session_state['report_type'] = None
 
     # Confiracoes da pagina
     st.set_page_config(
         page_title="Página principal",
-        layout="centered",
+        layout="wide",
         initial_sidebar_state="collapsed",
     )
-    st.title('Essa é a página principal!')
 
-    # Menu principal para os lideres
-    with st.container(border=True):
-        match st.session_state.user_type:
-            # Menu do líder
-            case 'LIDER':
-                st.header('Seja bem-vindo líder! Aqui você pode administrar suas facções.')
-                st.session_state['report_type'] = 'facções'
+    match st.session_state.user_type:
+        # Menu do comandante
+        case 'COMANDANTE':
+            st.header('Seja bem-vindo comandante! Aqui você pode administrar sua nação.')
+            st.session_state['report_type'] = ['nação']
 
-            # Menu do comandante
-            case 'COMANDANTE':
-                st.header('Seja bem-vindo comandante! Aqui você pode administrar suas comunidades.')
-                st.session_state['report_type'] = 'nação'
+        # Menu do oficial
+        case 'OFICIAL':
+            st.header('Seja bem-vindo oficial! Aqui você pode administrar suas habitações.')
+            st.session_state['report_type'] = ['habitações']
 
-            # Menu do oficial
-            case 'OFICIAL':
-                st.header('Seja bem-vindo oficial! Aqui você pode administrar suas habitações.')
-                st.session_state['report_type'] = 'habitações'
+        # Menu do cientista
+        case 'CIENTISTA':
+            st.header('Seja bem-vindo cientista! Aqui você pode administrar os registros de estrelas.')
+            st.session_state['report_type'] = ['estrelas']
 
-            # Menu do cientista
-            case 'CIENTISTA':
-                st.header('Seja bem-vindo cientista! Aqui você pode administrar os registros de estrelas.')
-                st.session_state['report_type'] = 'estrelas'
+    # Caso o líder também seja líder de alguma facção
+    if st.session_state.faccao:
+        st.session_state['report_type'].append('facções')
+    
+    # Exibindo informações do usuário
+    name = 'Placeholder'
+    st.markdown(f"<h6 color: white;'>Usuário autenticado: ***{st.session_state.cpi[3:11]}-**</h6>", unsafe_allow_html=True)
+    st.markdown(f"<h6 color: white;'>Nome: {name}</h6>", unsafe_allow_html=True)
         
-        st.container(height=5, border=False)
-        
-        _, col2, _, col4, _ = st.columns([8, 10, 5, 10, 8])
+    # Botões da página
+    st.container(height=5, border=False)
+    col1, col2, col3, col4 = st.columns(4)
+    # Botao de gerar relatorio
+    with col1:
+        if st.button('Gerar relatórios'):
+            st.switch_page('pages/reports_page.py')
 
-
-        # Botao de gerar relatorio
-        with col2:
-            if st.button('Gerar relatórios'):
-                st.switch_page('pages/reports_page.py')
-
-        # Botao de gerenciamento
-        with col4:
-            if st.session_state.user_type == 'OFICIAL':
-                disabled=True
-            else:
-                disabled=False
-                
-            if st.button(f'Gerenciar {st.session_state.report_type}', disabled=disabled):
-                st.switch_page('pages/management_page.py')
-
-    _, col2, _ = st.columns([20, 5, 20])
-
-    # Botao de sair do aplicativo
+    # Botao de gerenciamento
     with col2:
-        if st.button('Sair', type='primary'):
+        if st.session_state.user_type == 'OFICIAL':
+            disabled=True
+        else:
+            disabled=False
+            
+        if st.button(f'Gerenciar {st.session_state.report_type[0]}', disabled=disabled):
+            st.switch_page('pages/management_page.py')
+
+    # Botao de gerar relatorios específico para líderes de facção
+    with col3:
+        if len(st.session_state.report_type) == 2:
+            disabled=True
+        else:
+            disabled=False
+
+        if st.button(f'Gerar relatórios de facção', disabled=disabled):
+            st.switch_page('pages/reports_leader.py')
+
+    # Botao de gerenciamento específico para líderes de facção
+    with col4:
+        if len(st.session_state.report_type) == 2:
+            disabled=True
+        else:
+            disabled=False
+
+        if st.button(f'Gerenciar facção', disabled=disabled):
+            st.switch_page('pages/management_leader.py')
+    
+    # Botao de sair do aplicativo
+    st.container(height=50, border=False)
+    if st.button('Sair', type='primary'):
             st.switch_page('login_page.py')
 
 if __name__ == '__main__':
